5cdaf2aa6ac504c31fe97bb4a3cd84738fddb2b1
Jay Helm <45608814+SgtBacon@users.noreply.github.com>
2021-03-03 10:34:53 -0800

    Initial commit

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    //On press, start game
+    public void StartGame()
+    {
+        Debug.Log("Start");
+        //SceneManager.LoadScene("PlayerSelect");
+    }
+
+    //On press, Quit Game
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+    //On press, open Options
+    public void Options()
+    {
+        Debug.Log("Options");
+    }
+}
