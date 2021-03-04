581385d9d16adb8a7ce6cf77e74762715e256f4f
Jay Helm <45608814+SgtBacon@users.noreply.github.com>
2021-03-04 10:28:25 -0800

    Basic character work

---
+++ Assets/Scripts/Char1Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Char1Controller : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 moveby = transform.right * x + transform.forward * z;
+
+        rb.MovePosition(transform.position + moveby.normalized * Speed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/SelectController.cs
+++ Assets/Scripts/SelectController.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SelectController : MonoBehaviour
 {
+
     //Select character based on Button being clicked
     //might change later to individual functionality
     //current goal is to have button pressed and any button has the same code to select that character in it's name
     public void CharacterSelect(Button b)
     {
         //PlayerSelect.pickplayer
+        Debug.Log(b.name);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MainMenuScene");
     }
 
     //TODO add function to let player pick a character and then send that character into the new scene
